8b5f2812ab808ea7f14f312d23f08f64cbea3b56
Mateus Romão <m.romao.ds@gmail.com>
2021-06-17 19:57:57 -0300

    Add params id

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const data = require('./server.json');
 
+const episodes = data.episodes;
+
 const app = express();
 
 const PORT = process.env.PORT || 8877;
 
+app.get(`/episodes/:id`, (req, res)=>{
+  res.json(episodes.find((episode)=>{
+    return episode.id = req.params.id
+  }));
+})
+
 app.get('/episodes', (req, res)=>{
-  res.json(data.episodes);
+  res.json(episodes);
 })
 
 app.get('/', (req, res)=>{
-  res.json(data);
+  res.send({
+    msg: "find episodes with: https://garticuno-api-podcastr.herokuapp.com/episodes"
+  });
 })
 
 app.listen(PORT, ()=>{
