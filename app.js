b0d777544040d7b6f34bc0b5bf0cece67bdb6a41
Mateus Romão <m.romao.ds@gmail.com>
2021-07-22 15:51:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,19 +8,21 @@ const app = express();
 const PORT = process.env.PORT || 8877;
 
 app.get(`/episodes/:id`, (req, res)=>{
-  res.json(episodes.find((episode)=>{
+  const json = episodes.find((episode)=>{
     return episode.id === req.params.id
-  }));
+  });
+
+  res.end(JSON.stringify(json, null, 4))
 })
 
 app.get('/episodes', (req, res)=>{
-  res.json(episodes);
+  res.end(JSON.stringify(episodes, null, 4))
 })
 
 app.get('/', (req, res)=>{
-  res.send({
+  res.end(JSON.stringify({
     msg: "find episodes with: https://garticuno-api-podcastr.herokuapp.com/episodes"
-  });
+  }, null, 4));
 })
 
 app.listen(PORT, ()=>{
